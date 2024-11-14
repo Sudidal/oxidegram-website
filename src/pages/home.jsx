20b6a334566af25059e9b76173a5235dd72c7cc4
oxide <mohammedhaider15mo@gmail.com>
2024-11-14 22:01:22 +0300

    Initial page layout

---
+++ src/pages/home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <div className="container">
+      <aside className="aside">
+        <div className="logo">Oxidegram</div>
+        <ul className="unstyled-list">
+          <li>Home</li>
+          <li>Search</li>
+          <li>Explore</li>
+          <li>Messages</li>
+          <li>Notifications</li>
+          <li>Create</li>
+        </ul>
+      </aside>
+      <main className="main">
+
+      </main>
+      <div className="right-side">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;

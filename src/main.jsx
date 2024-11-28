f717eafbf089df05d3a7bc64952273345c3b6b2a
oxide <mohammedhaider15mo@gmail.com>
2024-11-28 11:09:17 +0300

    Remove <app /> from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
-import App from "./app.jsx";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <App />
   </React.StrictMode>
 );

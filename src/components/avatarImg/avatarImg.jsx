2017990faa3662717f428cd139d81adb69b7ebfd
oxide <mohammedhaider15mo@gmail.com>
2024-12-13 21:51:08 +0300

    Polish avatarImg - better sizing  and alignment - display fallback image

--- src/components/avatarImg/avatarImg.jsx
+++ src/components/avatarImg/avatarImg.jsx
@@ -1,13 +1,30 @@
 import PropTypes from "prop-types";
-import classes from "./avatarImg.module.css"
+import classes from "./avatarImg.module.css";
 
 function AvatarImg({ url, width = 50 }) {
-  return <img style={{width: width + "px"}} className={classes.icon} src={url} alt="profile avatar" title="profile avatar" />;
+  const fallbackUrl = "/photos/anon-avatar.png";
+
+  if (!url) {
+    url = fallbackUrl;
+  }
+
+  return (
+    <div style={{ width: width + "px", height: width + "px" }}>
+      <img
+        className={classes.icon}
+        src={url}
+        onError={(ev) => {
+          ev.target.src = fallbackUrl;
+        }}
+        title="profile avatar"
+      />
+    </div>
+  );
 }
 
 AvatarImg.propTypes = {
   url: PropTypes.string,
-  width: PropTypes.number
-}
+  width: PropTypes.number,
+};
 
-export default AvatarImg
+export default AvatarImg;

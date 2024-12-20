ed78dcac950aeeb558beecad90a3d9e6ae86077a
oxide <mohammedhaider15mo@gmail.com>
2024-12-20 12:32:48 +0300

    Feat: follow, message prof btns r functional now

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../../api.js";
 import profileContext from "../../contexts/profileContext.js";
+import alertContext from "../../contexts/alertContext.js";
 import SidePanel from "../../components/sidePanel/sidePanel.jsx";
 import AvatarImg from "../../components/avatarImg/avatarImg.jsx";
 import SvgFileToInline from "../../components/svgFileToInline/svgFileToInline.jsx";
@@ -10,19 +11,23 @@ import Tabs from "../../components/tabs/tabs.jsx";
 import classes from "./profile.module.css";
 
 function Profile() {
-  const [reqProfile, setProfile] = useState(null);
-  const [detailedProfile, setDetailedProfile] = useState(null);
+  const [reqProfile, setReqProfile] = useState(null);
+  const [followed, setFollowed] = useState(null);
+  const [profileDetails, setProfileDetails] = useState(null);
   const profile = useContext(profileContext);
+  const alert = useContext(alertContext);
   const params = useParams();
-  const nav = useNavigate()
+  const nav = useNavigate();
 
   const profileId = params.profileId;
+  console.log(followed);
 
   useEffect(() => {
     api.getProfile(profileId).then((res) => {
-      setProfile(res.profile);
+      setReqProfile(res.profile);
       api.getDetailsOfOneProfile(profileId).then((res) => {
-        setDetailedProfile(res.profile);
+        setProfileDetails(res.profile);
+        setFollowed(res.profile.followed);
       });
     });
   }, [profileId]);
@@ -30,7 +35,7 @@ function Profile() {
   return (
     <div>
       <SidePanel />
-      {detailedProfile && (
+      {profileDetails && (
         <main className={`${classes.main} main-with-margin`}>
           <div className={classes.profileInfo}>
             <div>
@@ -39,27 +44,81 @@ function Profile() {
             <div className={classes.right}>
               <div className={classes.proTop}>
                 <p className="huge-text">{reqProfile.username}</p>
+                {reqProfile.id !== profile.id &&
+                  (followed ? (
+                    <button
+                      className="secondary-btn"
+                      onClick={() => {
+                        api.unfollow(reqProfile.id).then((res) => {
+                          alert.show(res.msg);
+                          if (res.ok) {
+                            setFollowed(false);
+                          }
+                        });
+                      }}
+                    >
+                      Unfollow
+                    </button>
+                  ) : (
+                    <button
+                      className="primary-btn"
+                      onClick={() => {
+                        api.follow(reqProfile.id).then((res) => {
+                          alert.show(res.msg);
+                          if (res.ok) {
+                            setFollowed(true);
+                          }
+                        });
+                      }}
+                    >
+                      Follow
+                    </button>
+                  ))}
                 {reqProfile.id !== profile.id && (
-                  <button className="primary-btn">Follow</button>
-                )}
-                {reqProfile.id !== profile.id && (
-                  <button className="secondary-btn">Message</button>
+                  <button
+                    className="secondary-btn"
+                    onClick={() => {
+                      api.addContact(reqProfile.id).then((res) => {
+                        if (res.ok) {
+                          nav("/messages");
+                        } else {
+                          alert.show(res.msg);
+                        }
+                      });
+                    }}
+                  >
+                    Message
+                  </button>
                 )}
                 {reqProfile.id === profile.id && (
-                  <button className="secondary-btn" onClick={() => {
-                    nav("/settings")
-                  }}>Edit profile</button>
+                  <button
+                    className="secondary-btn"
+                    onClick={() => {
+                      nav("/settings");
+                    }}
+                  >
+                    Edit profile
+                  </button>
                 )}
               </div>
               <div className={`big-text ${classes.proMiddle}`}>
                 <p>
-                  <span className="semibold-text">{detailedProfile.posts.length}</span> posts
+                  <span className="semibold-text">
+                    {profileDetails.posts.length}
+                  </span>{" "}
+                  posts
                 </p>
                 <p>
-                  <span className="semibold-text">{detailedProfile.followers.length}</span> followers
+                  <span className="semibold-text">
+                    {profileDetails.followers.length}
+                  </span>{" "}
+                  followers
                 </p>
                 <p>
-                  <span className="semibold-text">{detailedProfile.follows.length}</span> following
+                  <span className="semibold-text">
+                    {profileDetails.follows.length}
+                  </span>{" "}
+                  following
                 </p>
               </div>
               <div className={classes.proBottom}>
@@ -82,7 +141,7 @@ function Profile() {
                 iconPath: "/icons/grid.svg",
                 content: (
                   <div className={classes.posts}>
-                    {detailedProfile.posts.map((post) => {
+                    {profileDetails.posts.map((post) => {
                       return <PostOverview key={post.id} post={post} />;
                     })}
                   </div>
@@ -93,7 +152,7 @@ function Profile() {
                 iconPath: "/icons/save-small.svg",
                 content: (
                   <div className={classes.posts}>
-                    {detailedProfile.savedPosts.map((post) => {
+                    {profileDetails.savedPosts.map((post) => {
                       return <PostOverview key={post.id} post={post} />;
                     })}
                   </div>

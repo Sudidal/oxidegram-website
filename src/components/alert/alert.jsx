b95f2e49ac04d7c268c3dca46d6c1584b054cb31
oxide <mohammedhaider15mo@gmail.com>
2024-12-22 20:41:37 +0300

    FIX: some alert UI fixes

--- src/components/alert/alert.jsx
+++ src/components/alert/alert.jsx
@@ -3,26 +3,28 @@ import PropTypes from "prop-types";
 import classes from "./alert.module.css";
 
 function Alert({ callback }) {
-  const [msg, setMsg] = useState("");
-  const [curTimer, setCurTimer] = useState(null);
+  const [msg, setMsg] = useState(false);
+  const curTimer = useRef(null);
   const graphicRef = useRef(null);
-  const duration = 4000;
+  const duration = 4000; //ms
 
   callback({
     show: show,
     hide: hide,
   });
 
-  function show(msg) {
-    if (typeof msg === "object" || msg === "") return;
-    setMsg(msg);
-    clearTimeout(curTimer);
-    setCurTimer(setTimer());
-    graphicRef.current.classList.add(classes.show);
+  function show(input) {
+    if (typeof input === "object" || input === "") return;
+
+    setMsg(input);
+    clearTimeout(curTimer.current);
+    curTimer.current = setTimer();
+    resetPositionAndShow();
   }
+
   function hide() {
     // So hide() won't be called twice if timer is still
-    clearTimeout(curTimer);
+    clearTimeout(curTimer.current);
     graphicRef.current.classList.remove(classes.show);
   }
 
@@ -30,6 +32,14 @@ function Alert({ callback }) {
     return setTimeout(() => {
       hide();
     }, duration);
+  }
+
+  function resetPositionAndShow() {
+    graphicRef.current.style = "transition: none; transform: translateY(0);";
+    setTimeout(() => {
+      graphicRef.current.style = "";
+      graphicRef.current.classList.add(classes.show);
+    }, 100);
   }
 
   return (
@@ -40,7 +50,6 @@ function Alert({ callback }) {
 }
 
 Alert.propTypes = {
-  msg: PropTypes.string,
   callback: PropTypes.func,
 };
 

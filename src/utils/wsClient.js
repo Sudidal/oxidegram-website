55d8c502619e0f7d3d07333c25939b528cb252ab
oxide <mohammedhaider15mo@gmail.com>
2024-12-25 15:08:59 +0300

    FIX: ws use old auth headers

--- src/utils/wsClient.js
+++ src/utils/wsClient.js
@@ -2,22 +2,55 @@ import { io } from "socket.io-client";
 import storageManager from "./storageManager.js";
 
 class WSClient {
-  #socket = io(import.meta.env.VITE_WS_URL, {
-    autoConnect: false,
-    path: "/direct",
-    extraHeaders: { authorization: storageManager.getAuthenticationKey() },
-  });
+  #socket = io();
+  #checkCooldown = 1000; //ms
+  #intervalId = null;
+  #AUTH_HEADER = "authorization";
+  #subscriptions = [];
 
   constructor() {
-    this.#socket.connect();
+    this.#connect();
   }
 
   subscribe(event, callback) {
-    this.#socket.on(event, callback);
+    this.#subscriptions.push({ event, callback });
+    this.setSubscriptions();
   }
 
   send(event, msg, args) {
     this.#socket.emit(event, msg, ...args);
+  }
+
+  #connect() {
+    console.log("Connecting to WebSocket server");
+    this.#socket.disconnect();
+    this.#socket = io(import.meta.env.VITE_WS_URL, {
+      path: "/direct",
+      extraHeaders: {
+        [this.#AUTH_HEADER]: storageManager.getAuthenticationKey(),
+      },
+    });
+
+    this.setSubscriptions();
+
+    clearInterval(this.#intervalId);
+    this.#intervalId = setInterval(() => {
+      this.checkForInfoUpdates();
+    }, this.#checkCooldown);
+  }
+
+  setSubscriptions() {
+    this.#socket.removeAllListeners();
+    this.#subscriptions.forEach((sub) => {
+      this.#socket.on(sub.event, sub.callback);
+    });
+  }
+
+  checkForInfoUpdates() {
+    const curAuthKey = storageManager.getAuthenticationKey();
+    if (curAuthKey !== this.#socket.io.opts.extraHeaders[this.#AUTH_HEADER]) {
+      this.#connect();
+    }
   }
 }
 
